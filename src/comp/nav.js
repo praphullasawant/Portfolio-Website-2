7247f0a6f2db515401c6ad411eefa7a7b34af37b
Praphulla Sawant <praphullasawant123@gmail.com>
2023-08-06 12:27:15 +0530

    navigation bar completed

---
+++ src/comp/nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <>
+      <div className='nav_container'>
+        <div className='logo'>
+            <h2>PS</h2>
+        </div>
+        <div className='links'>
+            <ul>
+                <li>
+                    <Link to='/' className='link'>Home</Link>
+                    <Link to='/about' className='link'>About</Link>
+                    <Link to='/work' className='link'>Work</Link>
+                    <Link to='/contact' className='link'>Contact</Link>
+                </li>
+            </ul>
+        </div>
+        <div className='btn'>
+            <Link to='/contact' className='btnlink'>Contact Me</Link>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Nav
